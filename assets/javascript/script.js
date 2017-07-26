e38845ca8bf7ee678c1457ef601446bcf8d85d11
Isaac Kwon <you@example.com>
2017-07-25 23:58:04 -0700

    2nd Commit: Basic Mechanics

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,52 @@
-var contents = [];
+//Array of contents//
+var contents = ["cat", "dog", "bear"];
 
+//Generate buttons from the contents array//
 function generateButton() {
-    var btn = $("<button>" + userSearch + "</button>");
+  $("#keyWords").empty();
+  for (var i = 0; i < contents.length; i++) {
+    var btn = $("<button>");
+    btn.addClass("image");
+    btn.attr("data-name", contents[i]);
+    btn.text(contents[i]);
     $("#keyWords").append(btn);
+  }
 }
 
+//Gets the gif images from GIPHY API using AJAX//
+function getImages() {
+  var userSearch = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userSearch + "&limit=10&api_key=dc6zaTOxFJmzC";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    for (var i = 0; i < response.data.length; i++) {
+      var newDiv = $("<div>");
+      $("#images").append(newDiv);
+      var showRating = $("<div>");
+      showRating.addClass("rating");
+      image = response.data[i].images.downsized.url;
+      rating = response.data[i].rating;
+      newDiv.append("<img src=" + image + ">");
+      newDiv.append(showRating);
+      showRating.text("Rating: " + rating);
+    }
+  });
+}
 
 $(document).ready(function() {
 
-  $("#button").on("click", function() {
-    userSearch = document.getElementById("search").value;
+  $("#button").on("click", function(event) {
+    event.preventDefault();
+    var userSearch = $("#search").val().trim();
     if (userSearch != "") {
-      generateButton();
       contents.push(userSearch);
+      generateButton();
     }
   });
+
+  $(document).on("click", ".image", getImages);
+  $("#images").empty();
+  generateButton();
 });
